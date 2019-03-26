bb5b9649d1d78160e99b9f5f44c60caedcd6f568
Ozaren <krishna.sd.2012@gmail.com>
2019-03-25 20:13:59 -0700

    initial implementation of solver consistency check
    existential quantification is not supported

---
+++ runner/src/main.rs
@@ -0,0 +1,231 @@
+
+use solver::*;
+
+#[derive(Clone, PartialEq, Eq, Hash)]
+pub enum Type {
+    Concrete(&'static str),
+    Infer(InfVar),
+    App(&'static str, Vec<Type>),
+}
+
+impl Type {
+    fn is_concrete(&self) -> bool {
+        match self {
+            Type::Infer(..) => false,
+            Type::Concrete(..) => true,
+            Type::App(_, x) => x.iter().fold(true, |acc, x| acc && x.is_concrete())
+        }
+    }
+    fn get_concrete(&self) -> Box<dyn Iterator<Item = Type>> {
+        match self {
+            Type::Infer(..) => Box::new(None.into_iter()),
+            Type::Concrete(..) => Box::new(Some(self.clone()).into_iter()),
+            Type::App(_, x) => Box::new(
+                x.clone()
+                    .into_iter()
+                    .flat_map(|x| x.get_concrete())
+                    .chain(
+                        if self.is_concrete() {
+                            Some(self.clone())
+                        } else {
+                            None
+                        }
+                    )
+            ),
+        }
+    }
+}
+
+#[allow(non_camel_case_types)]
+#[derive(Clone, PartialEq, Eq, Hash)]
+struct tc(Type, Type);
+
+impl std::fmt::Debug for tc {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{:?}: {:?}", self.0, self.1)
+    }
+}
+
+impl std::fmt::Debug for Type {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match &self {
+            Type::Concrete(x) => write!(f, "{}", x),
+            Type::Infer(x) => write!(f, "{:?}", x),
+            Type::App(x, rest) => write!(f, "{} {:?}", x, rest),
+        }
+    }
+}
+
+macro_rules! Type {
+    ($name:ident) => {
+        Type::Concrete(stringify!($name))
+    };
+    (@$name:ident) => {
+        Type::Infer($name)
+    };
+    ($name:ident $( $rest:tt )*) => {
+        Type::App(stringify!($name), Type![#vec new $($rest)*])
+    };
+    (#vec new $($rest:tt)*) => {{
+        let mut vec = Vec::new();
+
+        Type! { #vec vec $($rest)* }
+
+        vec
+    }};
+    (#vec $vec:ident) => {};
+    (#vec $vec:ident $a:ident $($rest:tt)*) => {
+        $vec.push( Type![ $a ] );
+        Type! { #vec $vec $($rest)* }
+    };
+    (#vec $vec:ident @$a:ident $($rest:tt)*) => {
+        $vec.push( Type![ @$a ] );
+        Type! { #vec $vec $($rest)* }
+    };
+    (#vec $vec:ident ($($inner:tt)*) $($rest:tt)*) => {
+        $vec.push( Type![ $($inner)* ] );
+        Type! { #vec $vec $($rest)* }
+    };
+}
+
+impl Predicate for tc {
+    type Item = Type;
+    type Iter = Box<dyn Iterator<Item = Type>>;
+
+    fn items(&self) -> Self::Iter {
+        self.0.get_concrete()
+    }
+
+    fn unify(&self, i: InfVar, r: &Self::Item) -> Self {
+        tc(
+            self.0.unify(i, r),
+            self.1.unify(i, r)
+        )
+    }
+}
+
+impl Type {
+    fn unify(&self, i: InfVar, r: &Self) -> Self {
+        match self {
+            &Type::Concrete(x)
+                => Type::Concrete(x),
+            &Type::App(x, ref rest)
+                => Type::App(x, rest.iter().map(|rule| rule.unify(i, r)).collect() ),
+            &Type::Infer(x)
+                => if x == i { r.clone() } else { Type::Infer(x) },
+        }
+    }
+}
+
+fn main() -> Result<(), ()> {
+    let mut solver = Solver::<tc>::new();
+    let mut ctx = Context::new();
+
+    add_rules! {
+        ctx in solver;
+
+        // Transitivity
+        // forall t {
+        //     forall u {
+        //         forall v {
+        //             (and ((cons t) => (cons u))
+        //                  ((cons u) => (cons v))) =>
+        //             ((cons t) => (cons u))
+        //         }
+        //     }
+        // }
+
+        // cons "u32: Copy".to_string();
+        
+        // cons "bool: Copy".parse::<tc>().unwrap();
+        // not (cons "bool: Clone".parse::<tc>().unwrap());
+
+        // forall t {
+        //     (cons format!("{:?}: Copy", t).parse::<tc>().unwrap()) =>
+        //     (cons format!("{:?}: Clone", t).parse::<tc>().unwrap())
+        // }
+
+        // not (cons tc(Type![Vec u32], Type![Default]));
+        // cons tc(Type![u32], Type![Sized]);
+        // cons tc(Type![f32], Type![Sized]);
+
+        cons tc(Type![Vec u32], Type![Default]);
+        not(cons tc(Type![Vec f32], Type![Default]));
+
+        exists t {
+            cons tc(Type![Vec @t], Type![Default])
+        }
+
+        exists t {
+            not(cons tc(Type![Vec @t], Type![Default]))
+        }
+
+        // forall t {
+        //     if (cons tc(Type![@t], Type![DynSized])) {
+        //         not(cons tc(Type![@t], Type![Sized]))
+        //     }
+        // }
+
+        // forall t {
+        //     if (cons tc(Type![@t], Type![Sized])) {
+        //         not(cons tc(Type![@t], Type![DynSized]))
+        //     }
+        // }
+
+        // forall t {
+        //     if (cons tc(Type![@t], Type![Sized])) {
+        //         cons tc(Type![Vec @t], Type![Sized])
+        //     }
+        // }
+
+        // forall t {
+        //     if (cons tc(Type![@t], Type![Sized])) {
+        //         not (cons tc(Type![Vec @t], Type![Copy]))
+        //         // and (not (cons tc(Type![Vec @t], Type![Copy])))
+        //         //     (cons tc(Type![Vec @t], Type![Sized]))
+        //     }
+        // }
+
+        // cons tc(Type![u32], Type![Sized]);
+        // cons tc(Type![f32], Type![Sized]);
+        // cons tc(Type![bool], Type![Sized]);
+        // cons tc(Type![char], Type![Sized]);
+
+        // not(cons tc(Type![Vec (Vec (Vec bool))], Type![Sized]));
+        // not(cons tc(Type![Vec u32], Type![Sized]));
+        
+        // not(cons tc(Type![Vec (Vec u32)], Type![Default]))
+
+        // not(cons tc(Type![Vec char], Type![Sized]));
+
+        // forall t {
+        //     (cons format!("{:?}: Clone", t)) => (cons format!("Vec {:?}: Clone", t))
+        // }
+
+        // not(
+        //     exists t {
+        //         cons format!("Vec {:?}: Copy", t)
+        //     }
+        // )
+    }
+
+    // let check: Rule<_> = rule!(
+    //     ctx cons "Vec u32: Clone".to_string()
+    // ).into();
+    
+    println!("{:#?}", solver);
+    // println!("{:#?}", check);
+
+    if let Some(token) = solver.is_consistent() {
+        // let rule = rule!(ctx cons tc(Type![u32], Type![Sized]));
+
+        // println!("{:#?}", solver.is_consistent_with_rule(rule.into(), &token));
+        println!("CONSISTENT");
+    } else {
+        println!("NOT CONSISTENT");
+    }
+
+    // solver.check(&check);
+
+    Ok(())
+}
---
+++ src/check.rs
@@ -0,0 +1,250 @@
+
+use super::{Solver, Rule, InfVar, Quant, Predicate};
+
+use std::collections::{VecDeque, HashMap, HashSet};
+use std::hash::Hash;
+
+#[derive(Debug)]
+struct RuleSet<T: Hash + Eq> {
+    t: HashSet<T>,
+    f: HashSet<T>
+}
+
+impl<T: Hash + Eq> std::ops::Index<bool> for RuleSet<T> {
+    type Output = HashSet<T>;
+
+    fn index(&self, a: bool) -> &HashSet<T> {
+        if a {
+            &self.t
+        } else {
+            &self.f
+        }
+    }
+}
+
+impl<T: Hash + Eq> std::ops::IndexMut<bool> for RuleSet<T> {
+    fn index_mut(&mut self, a: bool) -> &mut HashSet<T> {
+        if a {
+            &mut self.t
+        } else {
+            &mut self.f
+        }
+    }
+}
+
+trait COpt<T>: Sized {
+    fn call_with<F: FnOnce(T)>(self, _: F) {}
+}
+
+struct CSome<T>(T);
+struct CNone;
+
+impl<T> COpt<T> for CSome<T> { fn call_with<F: FnOnce(T)>(self, f: F) { f(self.0) } }
+impl<T> COpt<T> for CNone {}
+
+pub struct Token<'a>(&'a ());
+
+impl<P: Predicate> Solver<P>
+where P: std::fmt::Debug, P::Item: std::fmt::Debug {
+    pub fn is_consistent(&self) -> Option<Token<'_>> {
+        if self.is_consistent_inner(CNone) {
+            Some(Token(&()))
+        } else {
+            None
+        }
+    }
+    
+    pub fn is_consistent_with_rule(&self, rule: Rule<P>, _: &Token<'_>) -> bool {
+        self.is_consistent_inner(CSome(rule))
+    }
+
+    fn is_consistent_inner<O: COpt<Rule<P>>>(&self, new_rule: O) -> bool {
+        let mut rules: VecDeque<_> = self.rules.iter().map(|x| (x.clone(), 1)).collect();
+
+        new_rule.call_with(|x| rules.push_back((x, 1)));
+
+        let mut concrete_rules = RuleSet {
+            t: HashSet::new(),
+            f: HashSet::new(),
+        };
+
+        let mut quantifier_forall = HashMap::new();
+        let mut quantifier_exists = HashMap::new();
+
+        let mut registered_items = HashSet::new();
+
+        for (rule, _) in rules.iter() {
+            registered_items.extend(rule.items());
+        }
+
+        let registered_items = dbg!(registered_items);
+        
+        if rules.len() == 1 { return true; }
+
+        while let Some((rule, count)) = rules.pop_front() {
+            if count > 1000 { dbg!(rules.len()); panic!(); } // for terminating outdated rules
+
+            println!("=================================================================\n");
+            match dbg!(rule) {
+                Rule::True(x) => {
+                    dbg!("true");
+
+                    if concrete_rules[false].contains(&x) {
+                        dbg!(concrete_rules);
+                        return false;
+                    }
+
+                    concrete_rules[true].insert(x);
+                },
+
+                Rule::False(x) => {
+                    dbg!("false");
+                    if concrete_rules[true].contains(&x) {
+                        dbg!(concrete_rules);
+                        return false;
+                    }
+
+                    concrete_rules[false].insert(x);
+                },
+
+                Rule::And(box [x, y]) => {
+                    dbg!("and");
+
+                    rules.push_back((x, 1));
+                    rules.push_back((y, 1));
+                },
+
+                Rule::Quantifier(Quant::ForAll, t, box rule) => {
+                    dbg!("quantifier forall");
+
+                    let iter = if let Some(iter) = quantifier_forall.get_mut(&rule) {
+                        iter
+                    } else {
+                        quantifier_forall.entry(rule.clone())
+                                          .or_insert_with(|| registered_items.clone().into_iter())
+                    };
+                    
+                    if let Some(item) = iter.next() {
+                        rules.push_back((rule.unify(t, &item), 1));
+
+                        rules.push_back((
+                            Rule::Quantifier(Quant::ForAll, t, Box::new(rule)),
+                            count
+                        ));
+                    } else {
+                        quantifier_forall.remove(&rule);
+                    }
+                },
+
+                Rule::Quantifier(Quant::Exists, t, box rule) => {
+                    unimplemented!();
+                    dbg!("quantifier exists");
+                    
+                    let iter = if let Some(iter) = quantifier_exists.get_mut(&rule) {
+                        iter
+                    } else {
+                        quantifier_exists.entry(rule.clone())
+                                          .or_insert_with(|| registered_items.clone().into_iter())
+                    };
+                    
+                    if let Some(item) = iter.next() {
+                        rules.push_back((rule.unify(t, &item), 1));
+
+                        rules.push_back((
+                            Rule::Quantifier(Quant::Exists, t, Box::new(rule)),
+                            count
+                        ));
+                    } else {
+                        quantifier_exists.remove(&rule);
+                    }
+                },
+
+                Rule::Implication(box [a, b]) => {
+                    dbg!("implication");
+                    if a.is_true(&concrete_rules) {
+                        // ~(a -> b) === a and ~b
+                        if b.is_false(&concrete_rules) {
+                            dbg!(concrete_rules);
+                            return false;
+                        }
+                        // If it is true, then push the implication onto
+                        // the queue, to be processed later
+                        rules.push_back((b, 1))
+                    } else if a.is_false(&concrete_rules) {
+                        // If it is false, then it doesn't matter what b is
+                    } else if !rules.is_empty() {
+                        // If a is not determined (some unknown variables)
+                        // then kick it down the queue, only if
+                        // there are other rules to process
+                        // if there are no other rules, this there will be
+                        // no progress to be made, as it is impossible to
+                        // prove or disprove, so assume it is true
+                        rules.push_back((
+                            Rule::Implication(Box::new([a, b])),
+                            count + 1
+                        ));
+                    }
+                },
+            }
+            concrete_rules = dbg!(concrete_rules);
+        }
+
+        true
+    }
+}
+
+impl<P: Predicate> Rule<P>
+where P: std::fmt::Debug, P::Item: std::fmt::Debug {
+    fn unify(&self, inf_var: InfVar, item: &P::Item) -> Self {
+        println!("----------------");
+        let v = dbg!(inf_var);
+        let i = dbg!(item);
+        let this = dbg!(self);
+
+        let output = match this {
+            Rule::True(x) => Rule::True(x.unify(v, i)),
+            Rule::False(x) => Rule::False(x.unify(v, i)),
+            Rule::Implication(box [a, b]) => Rule::Implication(Box::new([a.unify(v, i), b.unify(v, i)])),
+            Rule::And(box [a, b]) => Rule::And(Box::new([a.unify(v, i), b.unify(v, i)])),
+            Rule::Quantifier(..) => unimplemented!()
+        };
+
+        let output = dbg!(output);
+
+        println!("----------------");
+
+        output
+    }
+
+    fn is_true(&self, set: &RuleSet<P>) -> bool {
+        match dbg!(self) {
+            Rule::True(x) => set[true].contains(x),
+            Rule::False(x) => set[false].contains(x),
+            Rule::Implication(box [a, b]) => if a.is_true(set) { b.is_true(set) } else { true },
+            Rule::And(box [a, b]) => a.is_true(set) && b.is_true(set),
+            Rule::Quantifier(..) => unimplemented!()
+        }
+    }
+
+    fn is_false(&self, set: &RuleSet<P>) -> bool {
+        match dbg!(self) {
+            Rule::True(x) => set[false].contains(x),
+            Rule::False(x) => set[true].contains(x),
+            Rule::Implication(box [a, b]) => a.is_true(set) && b.is_false(set),
+            Rule::And(box [a, b]) => a.is_false(set) || b.is_false(set),
+            Rule::Quantifier(..) => unimplemented!()
+        }
+    }
+
+    fn items<'a>(&'a self) -> Box<dyn 'a + Iterator<Item = P::Item>> {
+        match dbg!(self) {
+            | Rule::True(x)
+            | Rule::False(x) => Box::new(x.items()),
+
+            | Rule::Implication(box [a, b])
+            | Rule::And(box [a, b]) => Box::new(a.items().chain(b.items())),
+
+            Rule::Quantifier(_, _, box rule) => rule.items()
+        }
+    }
+}
